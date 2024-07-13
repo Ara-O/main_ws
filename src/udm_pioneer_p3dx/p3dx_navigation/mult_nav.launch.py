2456237630145f5f73125f447e9fa5a17fbf1ca7
Ara-O <oladipoeyiara@gmail.com>
2024-07-13 19:12:05 -0400

    ?

--- src/udm_pioneer_p3dx/p3dx_navigation/mult_nav.launch.py
+++ src/udm_pioneer_p3dx/p3dx_navigation/mult_nav.launch.py
@@ -1,31 +1,68 @@
+
 import os
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('udm_pioneer_p3dx'),
+            'p3dx_navigation',
+            'maps',
+            'corredor-ct9.yaml'))
 
-    gazebo_ros_pkg = get_package_share_directory('udm_pioneer_p3dx')
+    # param_dir = LaunchConfiguration(
+    #     'params',
+    #     default=os.path.join(
+    #         get_package_share_directory(''),
+    #         'param',
+    #         param_file_name))
 
-    movebase_robot1 = os.path.join(gazebo_ros_pkg, 'p3dx_gazebo', 'move_base_robot1.launch.py')
-    # movebase_robot2 = os.path.join(gazebo_ros_pkg, 'p3dx_gazebo','move_base_robot2.launch.py')
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'rviz',
+        'nav2_default_view.rviz')
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params',
+            default_value="/home/ara/main_ws/test2.yaml",
+            description='Full path to param file to load'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params': "/home/ara/main_ws/test2.yaml"}.items(),
+        ),
+
         Node(
             package='rviz2',
-            executable='rviz2',
-            name='rviz2node',
-            output='screen'
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([movebase_robot1]),
-            launch_arguments={'use_sim_time': 'true'}.items()
-        )
-
-        # IncludeLaunchDescription(
-        #     PythonLaunchDescriptionSource([movebase_robot2]),
-        #     launch_arguments={'use_sim_time': 'true'}.items()
-        # ),
+            node_executable='rviz2',
+            node_name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
     ])
